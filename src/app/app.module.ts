91e3178de1665f60eac51c5fdfdb2c4869433253
Justine <justinelo010@gmail.com>
2018-08-02 11:36:19 -0400

    added restaurant-home component and updated main page title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RestaurantSearchComponent } from './restaurant-search/restaurant-search.component';
+import { RestaurantHomeComponent } from './restaurant-home/restaurant-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RestaurantSearchComponent
+    RestaurantSearchComponent,
+    RestaurantHomeComponent
   ],
   imports: [
     BrowserModule
