4b66441e02238eab34b895ced66822c3e59f5d9a
kdelsener <delsener.k@husky.neu.edu>
2018-08-02 11:00:44 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RestaurantSearchComponent } from './restaurant-search/restaurant-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantSearchComponent
   ],
   imports: [
     BrowserModule
