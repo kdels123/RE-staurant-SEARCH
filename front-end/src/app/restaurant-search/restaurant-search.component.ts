41fe29c9ec218a64ef0cf7dab06288a410e1fed9
kdelsener <delsener.k@husky.neu.edu>
2018-08-06 17:28:07 -0400

    connect to yelp api

--- front-end/src/app/restaurant-search/restaurant-search.component.ts
+++ front-end/src/app/restaurant-search/restaurant-search.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantSearchComponent implements OnInit {
 
-  constructor() { }
+  token = 'Bearer _rSdKYm3xXCbQ_aE5Vw6q4xL5RxcwJgexzMo5-3-uLHBzDZCH_2xr0E_8C2oA4JHuHWckmGGRT0BvwF4vZzf-' +
+      'l26J5CYJ2U53n7SFNweH90tUfr37m717OiqSXxoW3Yx';
+
+    restaurants = [
+        {name: 'Legal Seafoods'},
+        {name: 'Barking Crab'},
+        {name: 'Babbo'}];
+
+    // requestObj = {
+    //     url: this.url,
+    //     data: {term: 'deli', latitude: 42.3601, longitude: -71.0589},
+    //     headers: {'Authorization': this.token}
+    // }
+
+    constructor() { }
 
   ngOnInit() {
+      fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=42.3601&longitude=-71.0589',
+          {headers: {'Authorization': this.token}})
+          .then(response => response.json())
+          .then(res => this.restaurants = res.businesses);
   }
 
 }
