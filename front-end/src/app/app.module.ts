c0575c5b4eed3a7971943b529412c41e386aa73b
kdelsener <delsener.k@husky.neu.edu>
2018-08-19 19:17:03 -0400

    bootstrap

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { RestaurantSearchComponent } from './restaurant-search/restaurant-search.component';
 import { RestaurantHomeComponent } from './restaurant-home/restaurant-home.component';
